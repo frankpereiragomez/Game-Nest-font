042e2f48061f4cb68741b9ba3aced21374ca71dd
Frank Pereira <frankpereirag000@gmail.com>
2023-05-31 09:37:26 +0200

    Add state to form inputs, enable the login button with the inputs filled

--- src/components/Form/LoginForm.tsx
+++ src/components/Form/LoginForm.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import FormStyled from "./FormStyled";
+import { UserCredentials } from "../../types";
 
 const LoginForm = (): React.ReactElement => {
+  const initialUserCredentials: UserCredentials = {
+    username: "",
+    password: "",
+  };
+
+  const [userCredentialsState, setUserCredentialsState] =
+    useState<UserCredentials>(initialUserCredentials);
+
+  const onChangeUserInputs = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUserCredentialsState({
+      ...userCredentialsState,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const isEnable =
+    userCredentialsState.username !== "" &&
+    userCredentialsState.password !== "";
+
   return (
     <FormStyled className="login-form">
       <div className="login-form__controls">
@@ -12,7 +32,9 @@ const LoginForm = (): React.ReactElement => {
           className="login-form__input"
           id="username"
           type="text"
+          onChange={onChangeUserInputs}
           placeholder="Enter your username"
+          value={userCredentialsState.username}
         />
       </div>
       <div className="login-form__controls">
@@ -23,10 +45,14 @@ const LoginForm = (): React.ReactElement => {
           className="login-form__input"
           id="password"
           type="password"
+          onChange={onChangeUserInputs}
           placeholder="Enter your password"
+          value={userCredentialsState.password}
         />
       </div>
-      <button className="login-form__button">Login</button>
+      <button className="login-form__button" type="submit" disabled={!isEnable}>
+        Login
+      </button>
     </FormStyled>
   );
 };
