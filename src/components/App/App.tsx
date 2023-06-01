045e8af5b42412dbccaa976cfaaca0d11af8424f
Frank Pereira <frankpereirag000@gmail.com>
2023-06-01 13:52:15 +0200

    Check the user's token in the local storage and loguin in if is the case

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import useLocalStorage from "../../hooks/useLocalStorage/useLocalStorage";
+import useToken from "../../hooks/useToken/useToken";
+import { useAppDispatch } from "../../store";
+import { loginUserActionCreator } from "../../store/user/userSlice";
 import Layout from "../Layout/Layout";
 
 const App = (): React.ReactElement => {
+  const { getToken } = useLocalStorage();
+  const { decodeToken } = useToken();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const token = getToken("token");
+    if (token) {
+      const decodeUserData = decodeToken(token);
+      dispatch(loginUserActionCreator(decodeUserData));
+    }
+  }, [decodeToken, dispatch, getToken]);
+
   return (
     <>
       <Layout />
