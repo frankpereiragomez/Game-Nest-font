a5d3e69b5148d0d481cdb39919e2ac8ae81e6c90
Frank Pereira <frankpereirag000@gmail.com>
2023-06-01 15:18:59 +0200

    Test the deleteToken function

--- src/hooks/useLocalStorage/useLocalStorage.test.ts
+++ src/hooks/useLocalStorage/useLocalStorage.test.ts
@@ -3,10 +3,10 @@ import useLocalStorage from "./useLocalStorage";
 import { realTokenMock } from "../../mocks/mockUser";
 
 describe("Given a useLocalStorage custom hook", () => {
+  const key = "token";
+
   describe("When it call the setToken function with a key and a token", () => {
     test("Then it should set the token in the local storage", () => {
-      const key = "token";
-
       const {
         result: {
           current: { setToken, getToken },
@@ -16,6 +16,20 @@ describe("Given a useLocalStorage custom hook", () => {
       setToken(key, realTokenMock.token);
 
       expect(getToken(key)).toBe(realTokenMock.token);
+    });
+  });
+
+  describe("When it call the delete function with a key", () => {
+    test("Then it should remove the token from the local storage", () => {
+      const {
+        result: {
+          current: { deleteToken },
+        },
+      } = renderHook(() => useLocalStorage());
+
+      deleteToken(key);
+
+      expect(localStorage.length).toBe(0);
     });
   });
 });
