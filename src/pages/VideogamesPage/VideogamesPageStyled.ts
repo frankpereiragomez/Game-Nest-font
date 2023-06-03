bbb88b7557848b741adc8b52bc38e7b40f6fb8f6
Frank Pereira <frankpereirag000@gmail.com>
2023-06-03 20:30:06 +0200

    Add VideogamePageStyled style

---
+++ src/pages/VideogamesPage/VideogamesPageStyled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const VideogamesPageStyled = styled.div`
+  background-color: ${({ theme }) => theme.colors.light_color};
+`;
+
+export default VideogamesPageStyled;
