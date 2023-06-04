3810a3cf0d7aa47dba5625326b1d678b4dbfe02d
Frank Pereira <frankpereirag000@gmail.com>
2023-06-04 16:33:40 +0200

    Add VideogameCardStyled styles

---
+++ src/components/VideogameCard/VideogameCardStyled.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const VideogameCardStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.soft_grey};
+  border-radius: 12px;
+  width: 270px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+
+  .videogame-card {
+    &__image {
+      object-fit: cover;
+      padding-block: 14px;
+    }
+
+    &__info-container {
+      display: flex;
+      flex-direction: column;
+      padding: 15px 10px;
+      border-radius: 0 0 12px 12px;
+      background-color: ${({ theme }) => theme.colors.medium_grey};
+      color: ${({ theme }) => theme.colors.light_color};
+      width: 270px;
+      height: 175px;
+      gap: 15px;
+    }
+
+    &__details-container p {
+      font-size: 18px;
+      font-family: ${({ theme }) => theme.fonts.tertiary};
+    }
+
+    &__name {
+      font-size: 20px;
+    }
+
+    &__details-container {
+      display: flex;
+      gap: 25px;
+    }
+
+    &__game-price {
+      font-size: 16px;
+    }
+  }
+`;
+
+export default VideogameCardStyled;
