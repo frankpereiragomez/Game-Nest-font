197754e82bc048d413ecd872742fd78df9683e7e
Frank Pereira <frankpereirag000@gmail.com>
2023-06-06 18:35:01 +0200

    Test UserFeedback new uses cases

--- src/components/Ui/UserFeedback/UserFeedback.test.tsx
+++ src/components/Ui/UserFeedback/UserFeedback.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../../utils/testUtils";
 import UserFeedback from "./UserFeedback";
+import userEvent from "@testing-library/user-event";
+import { store } from "../../../store";
 
 describe("Given a UserFeedback component", () => {
   describe("When it's rendered", () => {
@@ -12,6 +14,34 @@ describe("Given a UserFeedback component", () => {
       const modal = screen.getByLabelText(expectedLabelText);
 
       expect(modal).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered has an error modal", () => {
+    test("Then it should show an error modal with 'Error' title", () => {
+      const iconAltText = "error icon";
+
+      renderWithProviders(<UserFeedback />, { ui: { isError: true } });
+
+      const expectedModal = screen.getByAltText(iconAltText);
+
+      expect(expectedModal).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered has an error modal and the user clicks anywhere", () => {
+    test("Then it should close the error modal", async () => {
+      const buttonText = "close error button";
+
+      renderWithProviders(<UserFeedback />, { ui: { isError: true } });
+
+      const expectedModal = screen.getByRole("button", { name: buttonText });
+
+      await userEvent.click(expectedModal);
+
+      const storeState = store.getState();
+
+      expect(storeState.ui.showFeedback).toBeFalsy();
     });
   });
 });
