4f632d21a24fe66c8b793445123c6b26047d5ac2
Frank Pereira <frankpereirag000@gmail.com>
2023-06-07 12:37:54 +0200

    Test deleteVideogame mini reducer in videogamesSlice

--- src/store/videogame/videogameSlice.test.ts
+++ src/store/videogame/videogameSlice.test.ts
@@ -1,5 +1,8 @@
 import { getVideogamesDataMock } from "../../mocks/factories/videogames/videogamesFactory";
+import { videogamesCollectionMock } from "../../mocks/videogamesMocks";
 import {
+  VideogameState,
+  deleteVideogameActionCreator,
   initialVideogameState,
   loadVideogamesActionCreator,
   videogameReducer,
@@ -16,6 +19,25 @@ describe("Given a videogameReducer reducer", () => {
       );
 
       expect(newVideogamesState).toStrictEqual({ videogames: newVideogames });
+    });
+  });
+
+  describe("When it receives a current videogame state and a deleteVideogame actions with am id has payload", () => {
+    test("Then it should return a list of videogames without the video game with id received", () => {
+      const currentVideogameState: VideogameState = {
+        videogames: videogamesCollectionMock,
+      };
+
+      const expectedVideogameState: VideogameState = {
+        videogames: videogamesCollectionMock.slice(1),
+      };
+
+      const newVideogameState = videogameReducer(
+        currentVideogameState,
+        deleteVideogameActionCreator(videogamesCollectionMock[0].id)
+      );
+
+      expect(newVideogameState).toStrictEqual(expectedVideogameState);
     });
   });
 });
